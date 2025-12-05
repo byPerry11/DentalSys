1e5380ba5f003aa4bb0d0a384c8e0efaa626e26b
Mictte <chibimictte@gmail.com>
2025-12-04 19:37:37 -0800

    Falto el boton de hamburguesa en dentista Mictte

--- Presentacion/Views/DentistView.xaml.cs
+++ Presentacion/Views/DentistView.xaml.cs
@@ -21,7 +21,20 @@ namespace Presentacion.Views
         {
             InitializeComponent();
         }
-
+        private void BtnHamburger_Click(object sender, RoutedEventArgs e)
+        {
+            // Si la columna esta ancha, la colapsamos; si esta chica, la expandimos
+            if (SideMenuColumn.Width.Value > 100)
+            {
+                // Colapsar menu
+                SideMenuColumn.Width = new GridLength(80);
+            }
+            else
+            {
+                // Expandir menu
+                SideMenuColumn.Width = new GridLength(220);
+            }
+        }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
 
